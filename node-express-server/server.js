a0476582816244aeae03d4c95649093c6edf8ca2
hlopez130300 <hector.lopez@linkerfinance.com>
2025-11-30 21:48:55 -0600

    changed serverjs

--- node-express-server/server.js
+++ node-express-server/server.js
@@ -36,6 +36,6 @@ require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
